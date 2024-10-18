b29df12810e806592e420af15c1f204c07d6303c
Elijah Potter <me@elijahpotter.dev>
2024-10-18 11:37:59 -0600

    feat: added counter for obsidian plugin to waybar

---
+++ waybar/harper_obsidian_count.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+URL="https://api.github.com/repos/elijah-potter/harper-obsidian-plugin/releases?per_page=100"
+
+DATA=`curl -L "$URL" -H "Accept: application/vnd.github.v3+json"`
+
+# Below prints total for all binaries for all rleases.
+echo $DATA | jq '.[].assets[].download_count' | awk '{s+=$1} END {print s}'
