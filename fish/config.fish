8b181ad27a24477166c170e00cf7c803d4c3889b
Elijah Potter <me@elijahpotter.dev>
2025-09-05 14:01:04 -0600

    feat: updates

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ if not contains cargo $PATH
   fish_add_path $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer
   set -x EDITOR nvim
   set -x PAGER bat
+  set -x SCCACHE_CACHE_SIZE 25G
 end
 
 if status is-interactive
