efc621b1ce73358844e81aeb9478dc3f2a275c17
Elijah Potter <me@elijahpotter.dev>
2026-05-15 12:00:58 -0600

    feat: frontmatter snippet + tmux tweak

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -1,10 +1,58 @@
 local utils = require 'utils'
 
 local luasnip = require 'luasnip'
+local s = luasnip.snippet
+local f = luasnip.function_node
+local i = luasnip.insert_node
+local t = luasnip.text_node
+
 luasnip.config.setup({
   history = true,
   update_events = 'TextChanged,TextChangedI',
   enable_autosnippets = true,
+})
+
+luasnip.add_snippets('markdown', {
+  s({
+    trig = 'date',
+    dscr = 'Insert the current UTC date in RFC 1123 format',
+  }, {
+    f(function()
+      return os.date('!%a, %d %b %Y %H:%M:%S GMT')
+    end, {}),
+  }),
+  s({
+    trig = 'front',
+    dscr = 'Insert blog post frontmatter',
+  }, {
+    t({
+      '---',
+      '"description": "',
+    }),
+    i(1, 'Short description'),
+    t({
+      '"',
+      '"pubDate": "',
+    }),
+    f(function()
+      return os.date('!%a, %d %b %Y %H:%M:%S GMT')
+    end, {}),
+    t({
+      '"',
+      '"keywords":',
+      '  - "',
+    }),
+    i(2, 'keyword'),
+    t({
+      '"',
+      '"image": null',
+      '"featured": false',
+      '"draft": true',
+      '---',
+      '',
+    }),
+    i(0),
+  }),
 })
 
 utils.map("i", "<C-L>", function ()
