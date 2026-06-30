e03233c2de2bbb337548a75ca5e22e605e4264c1
Elijah Potter <me@elijahpotter.dev>
2026-06-30 07:42:13 -0600

    feat: set up hero text on the wallpaper

--- wallpapers/display_random.sh
+++ wallpapers/display_random.sh
@@ -6,13 +6,15 @@ cd /home/elijahpotter/.config/wallpapers
 SELECTED=$(find . -maxdepth 1 -type f -name "*.jpg" ! -name "output.jpg" | shuf -n 1)
 OUTPUT="./output.jpg"
 FONT="./Domine.ttf"
+HERO_TEXT_FILE="./hero_text.txt"
+HERO_TEXT=$(<"$HERO_TEXT_FILE")
 
 magick "$SELECTED" \
   -gravity center \
   -font "$FONT" \
   -pointsize 128 \
   -fill white \
-  -annotate +0+0 "Harden Harper Desktop" \
+  -annotate +0+0 "$HERO_TEXT" \
   "$OUTPUT"
 
 swaybg -m fill -i "$OUTPUT"
